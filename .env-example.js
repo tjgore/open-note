4f2e4c9f854fcc9472d3f412b8f93aa59f2bf456
TJ Gore <hello@tjwgore.com>
2019-10-13 00:52:42 -0400

    add env-example

---
+++ .env-example.js
@@ -0,0 +1,11 @@
+// rename to .env.js
+const firebaseEnvVariables = {
+    API_KEY: '"key"',
+    AUTH_DOMAIN: '"domain"',
+    DATABASE_URL: '"url"',
+    PROJECT_ID: '"code"',
+    STORAGE_BUCKET: '"bucket"',
+    MESSAGING_SENDER_ID: '"id"'
+  }
+  
+  module.exports = { firebaseEnvVariables }
